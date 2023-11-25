43419b1fe8c46336e48d8796770b339053936dcf
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-25 03:51:52 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+first_name = 'arshton'
+surname = 'Wangutusi'
+
+
+# capitalize changes the first character of a variable from small to upper case.
+full_name = first_name.capitalize() + ' ' + surname
+
+print(full_name)
+# len function is used for printing the length of a string.
+print(len(first_name))
+print(len(surname))
+print(len(full_name))
+print(full_name.endswith('Walyemwa')) #endswith methods checks if a string ends with the passed string.
