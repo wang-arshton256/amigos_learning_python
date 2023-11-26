91f65c905fa57044eb7f5b1f9a7a3134bf370075
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-26 03:38:01 +0300

    boolean expressions

---
+++ boolean.py
@@ -0,0 +1,7 @@
+wang = 25
+
+print(wang < 20)        #less than
+print(wang == 20)       #equal to   
+print(wang >= 20)       #greater than or equal to
+print(wang > 20)        #greater than
+print('wang' .endswith('x')) #check is it ends with
