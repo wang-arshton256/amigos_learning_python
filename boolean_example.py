7e1d7d35dfd814b23c2e63ad907d0d84ffaf4dd0
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-26 04:01:08 +0300

    boolean example 2

---
+++ boolean_example.py
@@ -0,0 +1,11 @@
+is_adult = True
+
+age = 18
+
+if is_adult:  # this means if adult is true print else if false dont print
+    print("is adult")
+
+if age >= 15:
+    print("adult")
+else:
+    print("not an adult")
