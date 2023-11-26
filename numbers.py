99383e7b8ae5de447f264ad9a5d0ec9b14b89204
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-26 03:30:11 +0300

    updated numbers

--- numbers.py
+++ numbers.py
@@ -2,9 +2,11 @@ addition = 10 + 5           # (+) addition
 subtraction = 10 - 5        # (-) subtraction
 multiplication = 10 * 5     # (*) multiplication
 division = 10 / 5           # (/) division
-# (%) modulas
+# (%) modulas means how many times a number is divisible by the other and the remainder it leaves.
+mod = 10 % 5
 
 print(addition)
 print(subtraction)
 print(multiplication)
 print(division)
+print(mod)
