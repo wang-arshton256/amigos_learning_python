ef5e27207fc0fb0b825fc7a30e507e73fb18975d
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-28 02:58:08 +0300

    while loops

---
+++ while_loop.py
@@ -0,0 +1,7 @@
+age = 25
+
+while age <= 35:
+    print(age)
+    age = age + 1
+else:
+    print('while loop ended and value of number is ' + str(age)) # + str() function concatenates the print statement with the string passed.
