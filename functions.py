16febfa042d1d5c08a532ae2420d86d56ce4fc1a
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-28 03:21:42 +0300

    functions

--- functions.py
+++ functions.py
@@ -1,11 +1,9 @@
-age = 13
-age2 = 45
-if age < 18:
-    print('oooops, you are below voting age.')
-else:
-    print('you are eligible to vote')
-    
-if age2 >= 75:
-    print('You are an elderly and should be allowed to vote first')
-else:
-    print('you are still a young adult')
+def check_age(age):
+    if age < 18:
+        print('oooops, you are below voting age.')
+    else:
+        print('you are eligible to vote')
+        
+check_age(30)
+check_age(77)
+check_age(45)
