24f98ed8f1e083febdbc9b3dd7e50b1a6a2ddddd
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-28 03:30:02 +0300

    functions example 2

---
+++ functions_2.py
@@ -0,0 +1,11 @@
+def Employee_Salary(salary):
+    if salary >= 1800000:
+        print('you are eligible to apply for a salary loan')
+    else:
+        print('you do not qualify for the salary loan benefits')
+
+
+Employee_Salary(500000)
+Employee_Salary(100000)
+Employee_Salary(2000000)
+Employee_Salary(1500000)
