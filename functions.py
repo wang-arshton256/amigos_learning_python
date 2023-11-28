192dba5eb4a9a8c7232b5c9a97d203dbe453923e
Wangutusi Arshton <wangarshton@gmail.com>
2023-11-28 03:07:23 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+age = 13
+age2 = 45
+if age < 18:
+    print('oooops, you are below voting age.')
+else:
+    print('you are eligible to vote')
+    
+if age2 >= 75:
+    print('You are an elderly and should be allowed to vote first')
+else:
+    print('you are still a young adult')
